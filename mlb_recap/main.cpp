e76a9009a747bfcf001dd192b4b508d7b531e790
Zach <zachstruck@gmail.com>
2020-01-09 19:14:24 -0700

    Make the GLFW a C++ object for initialization and deinitialization

--- mlb_recap/main.cpp
+++ mlb_recap/main.cpp
@@ -1,37 +1,75 @@
 #include <GLFW/glfw3.h>
 
+#include <cstdio>
 #include <cstdlib>
+#include <stdexcept>
+
+namespace
+{
+    class GlfwInit final
+    {
+    public:
+        GlfwInit()
+        {
+            if (glfwInit() == GLFW_FALSE)
+            {
+                throw std::runtime_error("Failed to initialize GLFW");
+            }
+        }
+
+        // Disable copy semantics
+        GlfwInit(GlfwInit const& rhs) = delete;
+        GlfwInit& operator=(GlfwInit const& rhs) = delete;
+
+        // Disable move semantics
+        GlfwInit(GlfwInit&& rhs) noexcept = delete;
+        GlfwInit& operator=(GlfwInit&& rhs) noexcept = delete;
+
+        ~GlfwInit()
+        {
+            glfwTerminate();
+        }
+    };
+}
 
 int main()
 {
-    /* Initialize the library */
-    if (!glfwInit())
-        return EXIT_FAILURE;
-
-    /* Create a windowed mode window and its OpenGL context */
-    GLFWwindow* window = glfwCreateWindow(640, 480, "Hello World", nullptr, nullptr);
-    if (window == nullptr)
+    try
     {
-        glfwTerminate();
+        /* Initialize the library */
+        GlfwInit const glfw;
+
+        /* Create a windowed mode window and its OpenGL context */
+        GLFWwindow* window = glfwCreateWindow(640, 480, "Hello World", nullptr, nullptr);
+        if (window == nullptr)
+        {
+            throw std::runtime_error("Failed to create a window");
+        }
+
+        /* Make the window's context current */
+        glfwMakeContextCurrent(window);
+
+        /* Loop until the user closes the window */
+        while (!glfwWindowShouldClose(window))
+        {
+            /* Render here */
+            glClear(GL_COLOR_BUFFER_BIT);
+
+            /* Swap front and back buffers */
+            glfwSwapBuffers(window);
+
+            /* Poll for and process events */
+            glfwPollEvents();
+        }
+    }
+    catch (std::exception const& ex)
+    {
+        std::printf("Error: %s\n", ex.what());
+    }
+    catch (...)
+    {
         return EXIT_FAILURE;
     }
 
-    /* Make the window's context current */
-    glfwMakeContextCurrent(window);
-
-    /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
-    {
-        /* Render here */
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        /* Swap front and back buffers */
-        glfwSwapBuffers(window);
-
-        /* Poll for and process events */
-        glfwPollEvents();
-    }
-
-    glfwTerminate();
     return EXIT_SUCCESS;
 }
