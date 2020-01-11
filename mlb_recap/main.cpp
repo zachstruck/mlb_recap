84696fd4a9c7348fdb9535f7188b5a95a7cef6f9
Zach <zachstruck@gmail.com>
2020-01-11 16:26:35 -0700

    Draw a triangle from the tutorial

--- mlb_recap/main.cpp
+++ mlb_recap/main.cpp
@@ -3,6 +3,7 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+#include <array>
 #include <cstdio>
 #include <cstdlib>
 #include <stdexcept>
@@ -57,7 +58,22 @@ int main()
 {
     try
     {
-        Mlb::MlbData const mblData = Mlb::getFeedData();
+        //Mlb::MlbData const mblData = Mlb::getFeedData();
+
+        const char* const vertexShaderSource =
+            "#version 330 core\n"
+            "layout (location = 0) in vec3 aPos;\n"
+            "void main()\n"
+            "{\n"
+            "    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0f);\n"
+            "}\0";
+        const char* const fragmentShaderSource =
+            "#version 330 core\n"
+            "out vec4 FragColor;\n"
+            "void main()\n"
+            "{\n"
+            "    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+            "}\n\0";
 
         // Initialize GLFW in this scope
         GlfwInit const glfw;
@@ -81,13 +97,101 @@ int main()
             throw std::runtime_error("Failed to initialize GLAD");
         }
 
+        // Build and compile shader program
+
+        auto const checkShaderCompile = [](GLuint shader)
+        {
+            GLint success;
+            glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+
+            if (success == GL_FALSE)
+            {
+                GLint logLength;
+                glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &logLength);
+
+                std::string infoLog(logLength, '\0');
+                glGetShaderInfoLog(shader, infoLog.size(), nullptr, infoLog.data());
+
+                throw std::runtime_error(infoLog);
+            }
+        };
+
+        auto const checkShaderLink = [](GLuint program)
+        {
+            GLint success;
+            glGetProgramiv(program, GL_LINK_STATUS, &success);
+
+            if (success == GL_FALSE)
+            {
+                GLint logLength;
+                glGetProgramiv(program, GL_INFO_LOG_LENGTH, &logLength);
+
+                std::string infoLog(logLength, '\0');
+                glGetShaderInfoLog(program, infoLog.size(), nullptr, infoLog.data());
+
+                throw std::runtime_error(infoLog);
+            }
+        };
+
+        // Vertex shader
+        GLuint const vertexShader = glCreateShader(GL_VERTEX_SHADER);
+        glShaderSource(vertexShader, 1, &vertexShaderSource, nullptr);
+        glCompileShader(vertexShader);
+        checkShaderCompile(vertexShader);
+
+        // Fragment shader
+        GLuint const fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+        glShaderSource(fragmentShader, 1, &fragmentShaderSource, nullptr);
+        glCompileShader(fragmentShader);
+        checkShaderCompile(fragmentShader);
+
+        // Link shaders
+        GLuint const shaderProgram = glCreateProgram();
+        glAttachShader(shaderProgram, vertexShader);
+        glAttachShader(shaderProgram, fragmentShader);
+        glLinkProgram(shaderProgram);
+        checkShaderLink(shaderProgram);
+
+        // This needs RAII
+        glDeleteShader(vertexShader);
+        glDeleteShader(fragmentShader);
+
+        // Some vertex data
+        std::array const vertices = {
+            -0.5f, -0.5f, 0.0f, // left
+            +0.5f, -0.5f, 0.0f, // right
+            +0.0f, +0.5f, 0.0f  // top
+        };
+
+        GLuint vbo, vao;
+        glGenVertexArrays(1, &vao);
+        glGenBuffers(1, &vbo);
+
+        glBindVertexArray(vao);
+
+        glBindBuffer(GL_ARRAY_BUFFER, vbo);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices.data(), GL_STATIC_DRAW);
+
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
+        glEnableVertexAttribArray(0);
+
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+        glBindVertexArray(0);
+
         // Loop until the user closes the window
         while (!glfwWindowShouldClose(window))
         {
             processInput(window);
 
             // Render
+            glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
             glClear(GL_COLOR_BUFFER_BIT);
+
+            // Draw a triangle
+            glUseProgram(shaderProgram);
+            glBindVertexArray(vao);
+            glDrawArrays(GL_TRIANGLES, 0, 3);
 
             // Swap front and back buffers
             glfwSwapBuffers(window);
